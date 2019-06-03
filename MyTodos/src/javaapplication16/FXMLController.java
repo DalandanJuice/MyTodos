50523065a4a283bcbfb79b6a97df7c79cff63f16
Genesis <genesistobiassargento@gmail.com>
2019-06-03 11:56:46 +0800

    Implements delete, add Clear button

--- MyTodos/src/javaapplication16/FXMLController.java
+++ MyTodos/src/javaapplication16/FXMLController.java
@@ -5,8 +5,8 @@ package javaapplication16;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 import java.awt.Checkbox;
+import java.awt.event.MouseListener;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +16,7 @@ import javafx.event.EventHandler;
 import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Side;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
@@ -42,44 +43,73 @@ public class FXMLController implements Initializable {
     @FXML
     private VBox todoBox;
     @FXML
+    private int i = 0;
+    @FXML
     private TextField textField1;
+    @FXML
     private ContextMenu menu;
+    @FXML
     private MenuItem delete;
+    @FXML
     private ArrayList<CheckBox> todos;
+    @FXML
+    private ArrayList<ContextMenu> cm;
+    @FXML
+    private int a;
+
     @FXML
     private void onKeyPressed(KeyEvent e) {
         if (e.getCode() == ENTER) {
-            todo.add(todoBox,todos,new CheckBox(textField1.getText()));
-            for(CheckBox cb: todos) {
-                cb.setContextMenu(menu);
+
+            todo.add(todoBox, todos, new CheckBox(textField1.getText()));
+           for (int j = 0; j < todos.size(); j++) {
+                  todos.get(j).setOnMouseClicked(this::showMenu);
             }
-        }
-    }
-    @FXML
-    private void delete(ActionEvent e) {
-        for(CheckBox cb: todos) {
-            if (cb.isSelected()) {
-                System.out.println("deleted");
-            }
+          
+
+            textField1.setText("");
         }
     }
     
+    private void showMenu(MouseEvent event) {
+        if (event.getButton() == SECONDARY) {
+                            for (int j = 0; j < todos.size(); j++) {
+                                if (event.getSource() == todos.get(j)) {
+                                    a = j;
+                                    menu.show(todos.get(j), Side.RIGHT, 0, 0);
+                                }
+                            }
+
+                        }
+    }
+
+    private void getSourced() {
+
+    }
+
     @FXML
-    
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        todos = new ArrayList<CheckBox>();
+         
+        a = -2;
+        todos = new ArrayList<>();
         menu = new ContextMenu();
         delete = new MenuItem("Delete");
         menu.getItems().add(delete);
         delete.setOnAction(new EventHandler<ActionEvent>() {
-    public void handle(ActionEvent e) {
-        System.out.println(e.getSource());
-    }
-});
-        
+            @Override
+            public void handle(ActionEvent event) {
+                for (int j = 0; j < todos.size(); j++) {
+                    if (a == j) {
+                        todoBox.getChildren().remove(todos.get(a));
+                        todos.remove(a);
+                    }
+                }
+            }
+
+        });
 
     }
 
